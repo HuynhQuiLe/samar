970368fa6e974c9c40945161af8bd32dde2c6ccb
HuynhQuiLe <lehuynhqui1996@gmail.com>
2023-07-02 23:54:14 +0700

    add scrolling effect

---
+++ src/js/main.js
@@ -0,0 +1,58 @@
+(function () {
+  // Back to Top - by CodyHouse.co
+  var backTop = document.getElementsByClassName("js-cd-top")[0],
+    offset = 300, // browser window scroll (in pixels) after which the "back to top" link is shown
+    offsetOpacity = 1200, //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+    scrollDuration = 700,
+    scrolling = false;
+
+  if (backTop) {
+    //update back to top visibility on scrolling
+    window.addEventListener("scroll", function (event) {
+      if (!scrolling) {
+        scrolling = true;
+        !window.requestAnimationFrame
+          ? setTimeout(checkBackToTop, 250)
+          : window.requestAnimationFrame(checkBackToTop);
+      }
+    });
+
+    //smooth scroll to top
+    backTop.addEventListener("click", function (event) {
+      event.preventDefault();
+      !window.requestAnimationFrame
+        ? window.scrollTo(0, 0)
+        : Util.scrollTo(0, scrollDuration);
+    });
+  }
+
+  function checkBackToTop() {
+    var windowTop = window.scrollY || document.documentElement.scrollTop;
+    windowTop > offset
+      ? Util.addClass(backTop, "cd-top--is-visible")
+      : Util.removeClass(backTop, "cd-top--is-visible cd-top--fade-out");
+    windowTop > offsetOpacity && Util.addClass(backTop, "cd-top--fade-out");
+    scrolling = false;
+  }
+})();
+
+// COUNTER UP
+$(".counter").counterUp();
+
+// FANCY BOX
+Fancybox.bind('[data-fancybox="gallery"]', {
+  // Your custom options
+});
+
+// SLICK CAROUSEL
+
+$(".blog__card").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: false,
+  swipeToSlide: true,
+});
+
+// WOW
+new WOW().init();
